ad52fd0ff954035209fcfd49e716e88c3b4f2e81
anthony <antcedrone95@gmail.com>
2021-05-20 09:45:50 -0400

    Updated colors and buttons

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 // VARIABLES
 let clickStatus = false;
-let colorChoice = "black";
-const colors = ['black', 'red', 'yellow', 'blue', 'white']
+let colorChoice = "lime";
+const colors = ['#90d83f', '#009FDC', '#B280D2', '#d64646', '#FFD700', '#10121a', 'slategray', 'white']
 
 
 // CREATE ELEMENTS
@@ -31,6 +31,10 @@ let buttonContainer = document.createElement('div');
 buttonContainer.className = "buttonContainer";
 document.body.append(buttonContainer);
 
+let toolsContainer = document.createElement('div');
+toolsContainer.className = "toolsContainer";
+document.body.append(toolsContainer);
+
 function makeButtons() {
   for (let j = 0; j < colors.length; j++) {
     let button = document.createElement('button');
@@ -44,17 +48,72 @@ function makeButtons() {
 }
 makeButtons();
 
+let changeWidth = document.createElement('button');
+changeWidth.className = "widthButton";
+changeWidth.textContent = "WIDTH"
+toolsContainer.append(changeWidth);
+
+changeWidth.addEventListener('click', function() {
+  let width = prompt("Enter pixel width: (max is 25)");
+  let div = document.querySelectorAll('.divs');
+  for (let i = 0; i < div.length; i++) {
+    if (width <= 25) {
+      div[i].style.width = width + "px";
+    } else {
+      alert("Width must be 25 or less");
+      break
+    }
+  }
+});
+
+let changeHeight = document.createElement('button');
+changeHeight.className = "heightButton";
+changeHeight.textContent = "HEIGHT"
+toolsContainer.append(changeHeight);
+
+changeHeight.addEventListener('click', function() {
+  let height = prompt("Enter pixel height: (max is 25)");
+  let div = document.querySelectorAll('.divs');
+  for (let i = 0; i < div.length; i++) {
+    if (height <= 25) {
+      div[i].style.height = height + "px";
+    } else {
+      alert("Height must be 25 or less");
+      break
+    }
+  }
+});
+
+let random = document.createElement('button');
+random.className = "randomButton";
+random.textContent = "RANDOM"
+toolsContainer.append(random);
+
+random.addEventListener('click', function() {
+  let div = document.querySelectorAll('.divs');
+  for (let i = 0; i < div.length; i++) {
+    let randomColor = colors[Math.floor((Math.random() * colors.length))];
+    div[i].style.backgroundColor = randomColor;
+  }
+});
+
 let reset = document.createElement('button');
 reset.className = "resetButton";
-reset.textContent = "Clear"
-buttonContainer.append(reset);
+reset.textContent = "RESET"
+toolsContainer.append(reset);
 
 reset.addEventListener('click', function() {
   let div = document.querySelectorAll('.divs');
-  for (let i = 0; i < draw.length; i++) {
-    div[i].style.backgroundColor = "";
-}
+  let answer = confirm("Are you sure?");
+  if (answer === true) {
+    for (let i = 0; i < draw.length; i++) {
+      div[i].style.backgroundColor = "";
+      div[i].style.height = "25px";
+      div[i].style.width = "25px";
+    }
+  }
 });
+
 
 
 // DRAWING
